fa2f7de833fb48aba316365662196ecc92576b57
Eitan August <eitanaugust@Eitans-MacBook-Pro.local>
2018-04-03 16:47:10 -0400

    Updated App

--- react-learning/src/App.js
+++ react-learning/src/App.js
@@ -1,7 +1,30 @@
 import React from 'react';
 import TextField from 'material-ui/TextField';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
 import Button from 'material-ui/Button';
 import axios from 'axios';
+
+
+var style = {
+    "Left": {
+        "marginLeft": "40px"
+    },
+
+    "HR": {
+        "marginLeft": "50px"
+    },
+    "HRs": {
+        "marginLeft": "70px"
+    },
+    "Button": {
+        "marginLeft": "20px"
+    },
+    "Time": {
+        "marginLeft": "85px"
+    }
+}
 
 class TextFieldExample extends React.Component {
     constructor() {
@@ -23,33 +46,46 @@ class TextFieldExample extends React.Component {
     }
 
     getHR = () => {
-        axios.get("http://vcm-3746.vm.duke.edu/api/heart_rate/" + this.state.user_email).then( (response) => {
+        axios.get("http://vcm-3746.vm.duke.edu:5000/api/heart_rate/" + this.state.user_email).then( (response) => {
             console.log(response);
             console.log(response.status);
-            //data.hrs????????
-            this.setState({"hrs": JSON.stringify(response.data.hrs)});
-            this.setState({"times": JSON.stringify(response.data.times)});
+            this.setState({"hrs": response.data.hrs});
+            this.setState({"times": response.data.times});
         })
     }
 
     render() {
+        var a =[<div><span style={style.HR}> </span> HR (BPM)<span style={style.HR}> </span> Time</div>];
+
+        for (var i = 0; i < this.state.hrs.length; i++) {
+            a.push((
+                <div>
+                    <span style={style.HRs}> </span>
+                {this.state.hrs[i]}
+                    <span style={style.Time}> </span>
+                {this.state.times[i]}
+                </div>
+            ));
+        }
+
         return (
             <div>
-                <AppBar position="static" style={styles.appBarStyle}>
+                <AppBar position="static">
                     <Toolbar>
-                        <Typography variant="title" color="inherit">
+                        <Typography variant="title" color="inherit" style={style.Left}>
                             Get Heart Rate Data
                         </Typography>
                     </Toolbar>
                 </AppBar>
-                <TextField
+                <TextField style={style.Left}
                     value={this.state.user_email}
                     onChange={this.onNameTextFieldChange}/>
-                <Button variant = "raised" color = "blue" onClick={this.getHR}>
+                <Button variant = "raised" color = "blue" style={style.Button} onClick={this.getHR}>
                     Get Data.
                 </Button>
-                {this.state.hrs /*show the current nameTextField state here in the browser */}
-                {this.state.times }
+                <div>
+                    {a}
+                </div>
             </div>
         );
     }
