c0b04c0f9a9edf835bece0091474f85f6398b819
Eitan August <eitanaugust@Eitans-MacBook-Pro.local>
2018-04-04 12:06:47 -0400

    Deleted extra files

--- test_service.py
+++
@@ -1,9 +0,0 @@
-import service
-import pytest
-
-
-def test_is_tachycardic(capsys):
-    assert service.is_tachycardic(135, 6) is True
-    assert service.is_tachycardic(110, 50) is True
-    assert service.is_tachycardic(160, 0.2) is False
-    assert service.is_tachycardic(140, 8) is True
